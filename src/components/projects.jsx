13bcb7edc45c34c81118f5386535ffc5d940817f
gprmarquez <gprmarquez@gmail.com>
2021-01-23 08:18:35 -0800

    added github icon links to projects section

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -20,7 +20,7 @@ export default class Projects extends Component {
 											<h3><a href="https://devpost.com/software/giv4gud" target="_blank">MLH Project: Giv4Gud</a></h3>
 											<span>Expo Application</span>
 											<p className="icon">
-												{/* <span><a href="#"><i className="icon-share3" /></a></span> */}
+												<span><a href="https://github.com/MLH-Fellowship/giv4gud" target="_blank"><i className="icon-github" /></a></span>
 												{/* <span><a href="#"><i className="icon-eye" /> 100</a></span> */}
 												{/* <span><a href="#"><i className="icon-heart" /> 49</a></span> */}
 											</p>
@@ -35,6 +35,7 @@ export default class Projects extends Component {
 											<h3><a href="https://devpost.com/software/freecoursex" target="_blank">MLH Project: freeCourseX</a></h3>
 											<span>Developer Tools</span>
 											<p className="icon">
+												<span><a href="https://github.com/MLH-Fellowship/FreeCourseX" target="_blank"><i className="icon-github" /></a></span>
 												{/* <span><a href="#"><i className="icon-share3" /></a></span> */}
 												{/* <span><a href="#"><i className="icon-eye" /> 100</a></span> */}
 												{/* <span><a href="#"><i className="icon-heart" /> 49</a></span> */}
@@ -50,6 +51,7 @@ export default class Projects extends Component {
 											<h3><a href="https://devpost.com/software/salary-io-nuohk0" target="_blank">MLH Project: Salary.io</a></h3>
 											<span>Machine Learning</span>
 											<p className="icon">
+												<span><a href="https://github.com/MLH-Fellowship/salary-io" target="_blank"><i className="icon-github" /></a></span>
 												{/* <span><a href="#"><i className="icon-share3" /></a></span> */}
 												{/* <span><a href="#"><i className="icon-eye" /> 100</a></span> */}
 												{/* <span><a href="#"><i className="icon-heart" /> 49</a></span> */}
@@ -65,8 +67,9 @@ export default class Projects extends Component {
 											<h3><a href="https://devpost.com/software/green-geese" target="_blank">MLH Project: Green-Geese</a></h3>
 											<span>Unity, Game Development</span>
 											<p className="icon">
+												<span><a href="https://github.com/MLH-Fellowship/Green-Geese" target="_blank"><i className="icon-github" /></a></span>
 												{/* <span><a href="#"><i className="icon-share3" /></a></span> */}
-												{/* <span><a href="#"><i className="icon-eye" /> 100</a></span> */}
+												<span><a href="https://zzranger.github.io/Green-Geese-Demo/" target="_blank"><i className="icon-eye" /> Play It!</a></span>
 												{/* <span><a href="#"><i className="icon-heart" /> 49</a></span> */}
 											</p>
 										</div>
@@ -77,9 +80,10 @@ export default class Projects extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/bestgeese.jpg)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="work.html">MLH Project: Best-Geese</a></h3>
+											<h3><a href="https://devpost.com/software/best-geese-vrycep">MLH Project: Best-Geese</a></h3>
 											<span>Education</span>
 											<p className="icon">
+												<span><a href="https://github.com/MLH-Fellowship/Best-Geese" target="_blank"><i className="icon-github" /></a></span>
 												{/* <span><a href="#"><i className="icon-share3" /></a></span> */}
 												{/* <span><a href="#"><i className="icon-eye" /> 100</a></span> */}
 												{/* <span><a href="#"><i className="icon-heart" /> 49</a></span> */}
@@ -95,6 +99,7 @@ export default class Projects extends Component {
 											<h3><a href="https://gprmarquez.github.io/Random-Quote/" target="_blank">Random Quote Generator</a></h3>
 											<span>Vanilla HTML, CSS, JavaScript</span>
 											<p className="icon">
+												<span><a href="https://github.com/gprmarquez/Random-Quote" target="_blank"><i className="icon-github" /></a></span>
 												{/* <span><a href="#"><i className="icon-share3" /></a></span> */}
 												{/* <span><a href="#"><i className="icon-eye" /> 100</a></span> */}
 												{/* <span><a href="#"><i className="icon-heart" /> 49</a></span> */}
