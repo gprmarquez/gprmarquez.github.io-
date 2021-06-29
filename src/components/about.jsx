867b567574d36808df15e31e44cb1d31eec94a77
gprmarquez <gprmarquez@gmail.com>
2021-06-29 01:18:56 -0500

    updating about to include MBSD

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,69 +1,69 @@
 import React, { Component } from 'react'
 
 export default class About extends Component {
-  render() {
-    return (
-      <div>
-        <section className="colorlib-about" data-section="about">
-        <div className="colorlib-narrow-content">
-            <div className="row">
-            <div className="col-md-12">
-                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
-                <div className="col-md-12">
-                    <div className="about-desc">
-                    <span className="heading-meta">About Me</span>
-                    <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I just graduated as a Software Engineering Fellow with <a href="https://fellowship.mlh.io/" target="_blank">Major League Hacking</a>, where my 10-person pod was split to work on various projects with a new theme every two weeks. Since graduating with a business management degree from <a href="https://business.ucr.edu/" target="_blank">UC Riverside</a> in 2018, I have been an Assistant Prospect Researcher at <a href="https://breakthrough.caltech.edu/" target="_blank">Caltech</a>, researching and recording information about 120K+ institute constituents in our <i>Ellucian Advance</i> database.</p>
+    render() {
+        return (
+            <div>
+                <section className="colorlib-about" data-section="about">
+                    <div className="colorlib-narrow-content">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                                    <div className="col-md-12">
+                                        <div className="about-desc">
+                                            <span className="heading-meta">About Me</span>
+                                            <h2 className="colorlib-heading">Who Am I?</h2>
+                                            <p>I just graduated as a Software Engineering Fellow with <a href="https://fellowship.mlh.io/" target="_blank">Major League Hacking</a>, where my 10-person pod was split to work on various projects with a new theme every two weeks. Since graduating with a business management degree from <a href="https://business.ucr.edu/" target="_blank">UC Riverside</a> in 2018, I just wrapped up my Assistant Prospect Researcher role at <a href="https://breakthrough.caltech.edu/" target="_blank">Caltech</a>, where I researched and recorded information about 120K+ institute constituents in our <i>Ellucian Advance</i> database. I will be joining the <a href="https://givetomedicine.uchicago.edu/" target="_blank">University of Chicago's Medicine and Biologial Sciences Division</a> as an Assistant Director of Strategic Research and Analysis.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div>
-        </section>
-        <section className="colorlib-about">
-        <div className="colorlib-narrow-content">
-            <div className="row">
-            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-                <span className="heading-meta">Skills so far</span>
-                <h2 className="colorlib-heading">Here is what I know</h2>
-            </div>
-            </div>
-            <div className="row row-pt-md">
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-1">
-                <span className="icon">
-                    <i className="icon-bulb" />
-                </span>
-                <div className="desc">
-                    <h3>Web Development </h3>
-                    <p>I have experience with HTML, CSS, JavaScript, Ruby, Python, MySQL. <br></br>Frameworks: React and Angular.</p>
-                </div>
-                </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-3">
-                <span className="icon">
-                    <i className="icon-phone3" />
-                </span>
-                <div className="desc">
-                    <h3>Creating & Deploying</h3>
-                    <p>Used <i>Unity</i> for game development. Explored Expo with React for app deployment</p>
-                </div>
-                </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-5">
-                <span className="icon">
-                    <i className="icon-bubble3" />
-                </span>
-                <div className="desc">
-                    <h3>Dev Ops</h3>
-                    <p>Experienced DevOps processes by designing and launching an automated tool for Gift Agreements using Microsoft PowerAutomate.</p>
-                </div>
-                </div>
-            </div>
-            {/*
+                </section>
+                <section className="colorlib-about">
+                    <div className="colorlib-narrow-content">
+                        <div className="row">
+                            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                                <span className="heading-meta">Skills so far</span>
+                                <h2 className="colorlib-heading">Here is what I know</h2>
+                            </div>
+                        </div>
+                        <div className="row row-pt-md">
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-1">
+                                    <span className="icon">
+                                        <i className="icon-bulb" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3>Web Development </h3>
+                                        <p>I have experience with HTML, CSS, JavaScript, Ruby, Python, MySQL. <br></br>Frameworks: React and Angular.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-3">
+                                    <span className="icon">
+                                        <i className="icon-phone3" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3>Creating & Deploying</h3>
+                                        <p>Used <i>Unity</i> for game development. Explored Expo with React for app deployment</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 text-center animate-box">
+                                <div className="services color-5">
+                                    <span className="icon">
+                                        <i className="icon-bubble3" />
+                                    </span>
+                                    <div className="desc">
+                                        <h3>Dev Ops</h3>
+                                        <p>Experienced DevOps processes by designing and launching an automated tool for Gift Agreements using Microsoft PowerAutomate.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            {/*
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-2">
                 <span className="icon">
@@ -98,10 +98,10 @@ export default class About extends Component {
                 </div>
             </div>
             */}
+                        </div>
+                    </div>
+                </section>
             </div>
-        </div>
-        </section>
-      </div>
-    )
-  }
+        )
+    }
 }
